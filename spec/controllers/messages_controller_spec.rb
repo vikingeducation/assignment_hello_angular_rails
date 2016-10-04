d672908ff6d7d8365541f8ab14b5d8ad86c5b91b
Hannah Squier <hannahsquier@gmail.com>
2016-10-04 10:56:09 -0700

    testing

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,30 +1,33 @@
 require 'rails_helper.rb'
 
-describe "GET /api/v1/messages.json", type: :controller do
+describe MessagesController do
+  describe "GET /api/v1/messages.json" do
 
-  let( :json ){ JSON.parse( response.body ) }
+    let( :json ){ JSON.parse( response.body ) }
 
-  before do
-    get '/api/v1/messages', format: :json
-  end
 
-  it 'should respond with a success' do
-    expect( response.status ).to eq( 200 )
-  end
+    before do
+      get 'index', format: :json
+    end
 
-  it 'returns a valid JSON object' do
-    expect( json ).to be_a Hash
-  end
+    it 'should respond with a success' do
+      expect( response.status ).to eq( 200 )
+    end
 
-  it 'returns an object with the post ID' do
-    expect( json.first.id ).to be 1
-  end
+    it 'returns a valid JSON object' do
+      expect( json ).to be_a Array
+    end
 
-  it 'returns an object with the posts body' do
-    expect( json.first.body ).to eq 'Hello Angular World!'
-  end
+    it 'returns an object with the post ID' do
+      expect( json.first["id"] ).to be 1
+    end
 
-  it 'returns the right keys' do
-    expect( json.body.keys ).to eq( [:id, :message] )
+    it 'returns an object with the posts body' do
+      expect( json.first["message"] ).to eq 'Hello Angular World!'
+    end
+
+    it 'returns the right keys' do
+      expect( json.first.keys ).to eq( ["id", "message"] )
+    end
   end
 end
