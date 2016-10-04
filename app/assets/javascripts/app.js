6c295ffdc6e5681c50743d07295f3c06047faa54
Leo Ahnn <leo.ahnn@gmail.com>
2016-10-04 12:25:14 -0400

    working on messages

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,36 @@
 var app = angular.module('app', ['ui.router', 'restangular']);
+
+
+app.factory('_', ['$window', function($window) {
+  return $window._;
+}]);
+
+app.config(
+  ["$httpProvider",
+  function($httpProvider) {
+    var token = $('meta[name=csrf-token]')
+      .attr('content');
+    $httpProvider
+      .defaults
+      .headers
+      .common['X-CSRF-Token'] = token;
+  }]);
+
+app.config(['RestangularProvider', function(RestangularProvider){
+
+
+
+}]);
+
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider
+    .state('messages', {
+        templateUrl: 'template/messages/index.html',
+        controller: MessagesCtrl,
+    })
+
+}])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#index' 
+
   get 'static_pages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
