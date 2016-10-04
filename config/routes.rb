b7ac850bb0853fb280a361bed85c0e35efeb6eda
Adrian Mui <adrian.mui@gmail.com>
2016-10-04 11:49:57 -0500

    messages api

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#index'
-  
+  resources :static_pages
+
   scope :api do
     scope :v1 do
-      resources :static_pages
+      resources :messages, only: [:index]
     end
   end
 end
