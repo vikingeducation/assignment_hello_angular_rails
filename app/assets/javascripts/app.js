1ea818440179464e164a3671833e7566362cd94f
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-24 15:50:13 -0700

    begin 'Configure the API'

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,49 @@
 var MyApp = angular.module('MyApp', ['ui.router', 'restangular']);
 
 
+
+
+MyApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("");
+
+  $stateProvider
+    .state('default', {
+      url: '',
+      views: {
+        "": {
+          templateUrl: 'templates/messages/index.html',
+          controller: 'MessagesCtrl'
+        }
+      }
+    })
+
+});
+
+
+
+
 MyApp.controller('TestCtrl', ['$scope', function($scope){
   $scope.message = 'Hello Angular on Rails!';
-}])
+}]);
+
+
+
+MyApp.controller('MessagesCtrl', ['$scope', function($scope){
+
+  $scope.hello = 'hello';
+
+}]);
+
+
+
+
+
+
+
+
+
+// debugging
+MyApp.run(function($rootScope){
+  $rootScope.$on("$stateChangeError", console.log.bind(console));
+});
