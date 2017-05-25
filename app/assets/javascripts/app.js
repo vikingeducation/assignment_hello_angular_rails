abe6b7c9b8b98c93aa85f9462c79f2b6e9c3d43a
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-24 17:03:21 -0700

    awesomeness!

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,13 @@
 var MyApp = angular.module('MyApp', ['ui.router', 'restangular']);
 
 
+MyApp.factory('_', ['$window', function($window) {
+  return $window._;
+}]);
 
 
 MyApp.config(function($stateProvider, $urlRouterProvider) {
-
-  $urlRouterProvider.otherwise("");
+  // $urlRouterProvider.otherwise("");
 
   $stateProvider
     .state('default', {
@@ -21,6 +23,15 @@ MyApp.config(function($stateProvider, $urlRouterProvider) {
 });
 
 
+MyApp.config(
+  ['RestangularProvider',
+  function(RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('/api/v1');
+    RestangularProvider.setRequestSuffix('.json');
+
+  }]);
+
 
 
 MyApp.controller('TestCtrl', ['$scope', function($scope){
@@ -29,10 +40,10 @@ MyApp.controller('TestCtrl', ['$scope', function($scope){
 
 
 
-MyApp.controller('MessagesCtrl', ['$scope', function($scope){
+MyApp.controller('MessagesCtrl', ['$scope', 'Restangular', function($scope, Restangular){
 
   $scope.hello = 'hello';
-
+  $scope.messages = Restangular.all('messages').getList().$object;
 }]);
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :static_pages
   root "static_pages#index"
+
+  # # creates routes like /api/v1/users
+  # # and path helpers like `users_path`
+  scope :api do
+    scope :v1 do
+      resources :messages
+    end
+  end
+
+
 end
